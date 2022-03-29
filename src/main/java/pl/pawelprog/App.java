28d9cba8ef8b832cfab2f5b091feafa7533657ec
pawelbogusz81 <pawelbogusz81@gmail.com>
2022-03-29 21:21:56 +0200

    Story US02

--- src/main/java/pl/pawelprog/App.java
+++ src/main/java/pl/pawelprog/App.java
@@ -1,7 +1,7 @@
 package pl.pawelprog;
 
 public class App {
-    public static void main(String[]args){
+    public static void main (String[] args){
 
         System.out.println("Witaj w Kole Fortuny!");
 
